b7d869172fe6fcc6cdfd9d11f6b01c01cc7ffada
Sergio Pérez <se.perezf@uniandes.edu.co>
2018-04-25 04:37:13 -0500

    Contenido

--- app/taller2/taller2/content_recommender.py
+++ app/taller2/taller2/content_recommender.py
@@ -20,4 +20,34 @@ def get_recommendation_by_review(review_id):
         print(list(db.business.find({'business_id': dfReviews.loc[i]['business_id_x']}))[0]['name'])
 
 
-get_recommendation_by_review('---0hl58W-sjVTKi5LghGw')
+def get_user_top_reviews(user_id):
+    
+    review_indexes = dfReviews.loc[dfReviews['user_id_x'] == 'zsZVg16yjZu5NIiS0ayjrQ'].sort_values('stars_x', ascending=False).head(10).index
+
+    businesess_count = {}
+    businesses_top = {}
+    for index in review_indexes:
+        cosine_similarities = cosine_similarity(tfidf_matrix[index], tfidf_matrix)
+        for i in cosine_similarities[0].argsort()[:-10:-1]:
+            business_id = dfReviews.loc[i]['business_id_x']
+            if businesess_count.get(business_id):
+                businesess_count[business_id] = businesess_count[business_id] + 1
+                businesses_top[business_id] = max(cosine_similarities[0][i], businesses_top[business_id])
+            else:
+                businesess_count[business_id] = 1
+                businesses_top[business_id] = cosine_similarities[0][i]
+            #print(list(db.business.find({'business_id': dfReviews.loc[i]['business_id_x']}))[0]['name'], cosine_similarities[0][i])
+            
+    top_businesses_count = {}
+    max_b = 10
+    i = 0
+    for business in sorted(businesess_count, key=businesess_count.get, reverse=True):
+        if i >= max_b:
+            break
+        if businesses_top[business] < 1:
+            top_businesses_count[business] = businesses_top[business]
+            i = i + 1
+
+    for business in sorted(top_businesses_count, key=top_businesses_count.get, reverse=True):
+        print(list(db.business.find({'business_id': business}))[0]['name'])
+get_user_top_reviews('zsZVg16yjZu5NIiS0ayjrQ')
--- app/taller2/taller2/svd_recommender.py
+++ app/taller2/taller2/svd_recommender.py
@@ -3,7 +3,12 @@ import pickle
 from surprise import SVD
 from surprise import Reader
 from surprise import Dataset
+from collections import defaultdict
 
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client['test']
 df = pd.read_csv( '../notebooks/toronto_reviews.csv')
 df2 = df[[ 'user_id', 'business_id', 'stars'] ]
 reader = Reader( rating_scale = ( 1, 5 ) )
@@ -14,3 +19,37 @@ mean = train_data.global_mean
 algo_pkl = open('../notebooks/svd_algo.pickle', 'rb')
 algo = pickle.load(algo_pkl)
 algo_pkl.close()
+
+
+def get_recommendations_for_user(user_id):
+
+    def get_top_n(predictions, n = 10):
+        # First map the predictions to each user.
+        top_n = defaultdict(list)
+        for uid, iid, true_r, est, _ in predictions:
+            top_n[uid].append((iid, est))
+
+        # Then sort the predictions for each user and retrieve the k highest ones.
+        for uid, user_ratings in top_n.items():
+            user_ratings.sort(key = lambda x: x[1], reverse=True)
+            top_n[uid] = user_ratings[:n]
+
+        return top_n
+
+    test = [x for x in train_data.build_testset() if x[0] == user_id]
+
+    predictions = algo.test(test)
+    
+    top_n = get_top_n(predictions, n = 10)
+    top_n = dict(top_n)
+    try:
+        return top_n[user_id]
+    except KeyError:
+        print('recommendations not found')
+        return {}
+
+
+recommendations = get_recommendations_for_user('zsZVg16yjZu5NIiS0ayjrQ')
+
+for recommendation in recommendations:
+    print(list(db.business.find({'business_id': recommendation[0]}))[0]['name'])
